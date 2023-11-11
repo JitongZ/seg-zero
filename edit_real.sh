2b013aa7edc7930a445a332ec184e372a3c65151
Jia Xiaodong <zengfanpu@gmail.com>
2023-11-12 00:49:33 +0800

    store scripts for running common things

---
+++ edit_real.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python src/edit_real.py \
+    --inversion "output/test_custom/inversion/stefano.pt" \
+    --prompt "output/test_custom/prompt/stefano.txt" \
+    --task_name "beard2clean shaven" \
+    --results_folder "output/test_custom/" 
---
+++ inversion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python src/inversion.py  \
+        --input_image "assets/test_images/custom/stefano.jpg" \
+        --results_folder "output/test_custom"
