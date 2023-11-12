815ad19836482a502119c09b4a1a419d149447b8
Fan Pu <zengfanpu@gmail.com>
2023-11-11 21:26:14 -0500

    update grab_data script

--- grab_data.sh
+++ grab_data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-image = "29954.jpg"
-mask_prefix = "29954"
+image=$1 # "29954.jpg"
+mask_prefix=$2 #"29954"
 
-cp "CelebAMask-HQ/CelebA-HQ-img/${image}" assets/custom/
-cp "CelebAMask-HQ/CelebAMask-HQ-mask-anno/${image}*" assets/masks
+copy_img="cp CelebAMask-HQ/CelebA-HQ-img/${image} assets/custom/"
+echo $copy_img
+$copy_img
+
+find CelebAMask-HQ/CelebAMask-HQ-mask-anno -type f -regex ".*${mask_prefix}.*" | xargs -I {} cp {} assets/masks/
